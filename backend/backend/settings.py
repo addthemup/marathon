5e1fac058cc06d99e19d9eb486c7725060152c3f
addthemup <adamwaynecarver@gmail.com>
2024-10-18 21:52:44 -0400

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,16 +107,6 @@ else:
 # Custom user model
 AUTH_USER_MODEL = 'users.UserProfile'
 
-if ENVIRONMENT == 'production':
-    SECURE_SSL_REDIRECT = True
-    SECURE_HSTS_SECONDS = 3600
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-
 
 # REST framework settings
 REST_FRAMEWORK = {
