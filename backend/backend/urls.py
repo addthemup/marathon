28b50001dad97268fe445d2f3fec74fb081ce21d
addthemup <adamwaynecarver@gmail.com>
2024-10-21 01:10:58 -0400

    backend docker

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-# Serve media files during development
+# Serve static and media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
