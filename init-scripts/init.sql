d60888cd8164847df2574cd92891c0c090a01d4d
addthemup <adamwaynecarver@gmail.com>
2024-10-25 15:29:43 -0400

    init.sql

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,3 +1,14 @@
-CREATE ROLE awc WITH LOGIN PASSWORD 'Starbury03' CREATEDB;
+-- init.sql
+DO $$ BEGIN
+   CREATE ROLE awc WITH LOGIN PASSWORD 'Starbury03';
+   RAISE NOTICE 'Role awc created';
+EXCEPTION
+   WHEN DUPLICATE_OBJECT THEN
+      RAISE NOTICE 'Role awc already exists';
+END $$;
+
 CREATE DATABASE marathon OWNER awc;
+RAISE NOTICE 'Database marathon created with owner awc';
+
 GRANT ALL PRIVILEGES ON DATABASE marathon TO awc;
+RAISE NOTICE 'Granted all privileges on database marathon to awc';
