590dbf10cb65a1594ccebe975c7bcb7cafc9b0e4
addthemup <adamwaynecarver@gmail.com>
2024-10-31 01:04:19 -0400

    nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,23 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Load environment variables for different environments
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   build: {
-    minify: false, // Disable esbuild minification
+    outDir: 'build',
+    minify: true, // Enable minification for production
   },
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // Expose the dev server on all network interfaces
     port: 5173,
     strictPort: true,  // Ensure the port doesn't fallback if 5173 is in use
-    hmr: {
-      host: '137.184.223.198',  // Your public IP
+    hmr: !isProduction && {
+      host: 'localhost',  // Use localhost for local development
       port: 5173,  // Ensure HMR runs on the correct port
     },
   },
-})
+});
