9f8a5831efc464050d93edfc9bef5b6841a1ba17
addthemup <adamwaynecarver@gmail.com>
2024-11-04 18:12:33 -0500

    package.json, vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    minify: false, // Disable esbuild minification
-  },
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Expose the dev server on all network interfaces
-    port: 5173,
-    strictPort: true,  // Ensure the port doesn't fallback if 5173 is in use
-    hmr: {
-      host: '137.184.223.198',  // Your public IP
-      port: 5173,  // Ensure HMR runs on the correct port
+  build: {
+    minify: false, // Disable minification for debugging
+    rollupOptions: {
+      external: ['@nivo/core'], // Externalize if the import is still causing issues
     },
   },
-})
+  server: {
+    host: '0.0.0.0', // Expose the dev server on all network interfaces
+    port: 5173,
+    strictPort: true, // Ensure the port doesn't fallback if 5173 is in use
+    hmr: {
+      host: '137.184.223.198', // Your public IP
+      port: 5173, // Ensure HMR runs on the correct port
+    },
+  },
+});
