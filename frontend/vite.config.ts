253f616c353b6063880035ed20eb9571ec73fbf7
addthemup <adamwaynecarver@gmail.com>
2024-11-04 16:58:51 -0500

    revert

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: true, 
+    minify: false, // Disable esbuild minification
   },
   plugins: [react()],
   server: {
