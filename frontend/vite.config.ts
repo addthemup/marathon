0de8731a4435a8ea710eab42d286f6e8cb665222
addthemup <adamwaynecarver@gmail.com>
2024-11-06 11:45:26 -0500

    frontend docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Check if the environment is development or production
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
+  base: isProduction ? '/marathon/' : '/',  // Set the base URL for production
   plugins: [react()],
   build: {
-    minify: isProduction, // Enable minification in production
+    minify: isProduction,
     rollupOptions: {
-      external: ['@nivo/core'], // Externalize large libs only if necessary
+      external: ['@nivo/core'],
     },
   },
   server: {
-    host: '0.0.0.0', // Expose the dev server on all network interfaces
+    host: '0.0.0.0',
     port: 5173,
-    strictPort: true, // Ensure it fails if the port is in use
+    strictPort: true,
     hmr: {
-      host: isProduction ? '137.184.223.198' : 'localhost', // Use localhost for local dev HMR
-      port: 5173, // Ensure HMR runs on the correct port
+      host: isProduction ? '137.184.223.198' : 'localhost',
+      port: 5173,
     },
   },
 });
