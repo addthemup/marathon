a17cf8e2aa087c393c298e925748af6284e14dfb
addthemup <adamwaynecarver@gmail.com>
2024-11-07 08:51:29 -0500

    new vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  base: isProduction ? '/marathon/' : '/',  // Set the base URL for production
+  base: '/',  // Use a simpler base path for now
   plugins: [react()],
   build: {
-    minify: isProduction,
-    rollupOptions: {
-      external: ['@nivo/core'],
-    },
+    minify: true,  // Keep minify on for production
   },
   server: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true,
-    hmr: {
-      host: isProduction ? '137.184.223.198' : 'localhost',
-      port: 5173,
-    },
   },
 });
